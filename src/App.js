88fb12e4366106857b0d069fd17d3c7590a57b8c
Bohdan <yurchenko.b.l@gmail.com>
2021-07-29 21:06:56 +0300

    added files for the project, markup and styles for the header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app__wrapper">
+        <div className='main__wrapper'>
+          <Header />
+          <div className='product__wrapper'>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import cartImg from '../assets/ico/cart.png';
+import logoImg from '../assets/img/logo.png';
+import '../css/header.scss';
+
+
+const burgerMenuBtn = () => {
+  const header = document.body.querySelector('.app-header');
+  header.classList.toggle('app-header--menu-open');
+};
+
+const cartOpenBtn = () => {
+  const main = document.body.querySelector('.app__wrapper');
+  main.classList.add('brightness');
+  const cart = document.body.querySelector('.cart');
+  cart.classList.add('app-header--cart-open');
+};
+function Header() {
+  
+  return (
+    <header className="app-header">
+      <button className="app-header__burger-btn" onClick={burgerMenuBtn}>
+        <span></span>
+        <span></span>
+      </button>
+      <div className="app-header__logo">
+        <img className="head__img" src={logoImg} alt="logo" />
+      </div>
+      <nav className="app-header__nav">
+        <ul className="app-header__nav-items">
+          <a href="/" className="app-header__link">Vikings</a>
+          <a href="/" className="app-header__link">Ancient Egypt</a>
+          <a href="/" className="app-header__link">Celtics</a>
+          <a href="/" className="app-header__link">Sale</a>
+        </ul>
+        <input className="app-header__search" type="search" />
+        <button className="app-header__cart-btn-open" onClick={cartOpenBtn}>
+          <img className="head__img" src={cartImg} alt="cart" />
+          <span id='header-cart-count'>{/* {this.props.sendInfoAboutCartItems.length} */}</span>
+        </button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
